eafff6aa92296d68f8d759aef15c53ee811c8319
romekb <49776445+romek-b@users.noreply.github.com>
2021-07-28 21:55:37 +0200

    cleanup

--- portal/src/app/resolvers/external-url.resolver.ts
+++ portal/src/app/resolvers/external-url.resolver.ts
@@ -5,9 +5,6 @@ import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/r
   providedIn: 'root'
 })
 export class ExternalUrlResolver implements Resolve<any> {
-  constructor() {
-      console.log('elo');
-  }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     window.location.href = route.data.url;
   }
